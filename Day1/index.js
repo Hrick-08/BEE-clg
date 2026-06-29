dc2219a6eb4d85f2586ae7913fe748142114fbbe
Hrick <hritabratadas8@gmail.com>
2026-06-29 21:05:16 +0530

    update

--- Day1/index.js
+++ Day1/index.js
@@ -1,10 +1,16 @@
 var btn = document.getElementById("submit")
-btn.addEventListener("click", handleClick)
+btn.addEventListener("click",handleClick)
 
-function handleClick() {
-    alert("button is triggered");
-    var imageInput = document.getElementById("url").value;
-    var titleInput = document.getElementById("title").value;
-    var ratingInput = document.getElementById("rating").value;
-    console.log(imageInput);
+function handleClick(){
+    var imageinput =  document.getElementById("url").value 
+    var titleInput = document.getElementById("title").value
+    var ratingInput = document.getElementById("rating").value
+    console.log(imageinput)
+    var carDiv = document.getElementById("carDiv")
+    carDiv.innerHTML += `
+    <div class="card">
+        <img src="${imageinput}" alt="">
+        <p>Movie : ${titleInput} </p>
+        <h3>Rating: ${ratingInput}</h3>
+    </div>`
 }
