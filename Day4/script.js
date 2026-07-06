9dd03ae0bac430c1ceb317a43c325791ebdeacef
Hrick <hritabratadas8@gmail.com>
2026-07-06 21:11:22 +0530

    day4

---
+++ Day4/script.js
@@ -0,0 +1,66 @@
+// const arr = [1,2,3,4,5];
+// arr.push(6);
+
+// const newArr = arr.map(num => num * 2);
+
+// const arr2 = arr.map(num => num);
+
+// console.log(arr);
+// console.log(newArr);
+// console.log(arr2);
+
+// chrome = v8 engine
+// internet explorer = chakra engine
+// firefox = spider monkey engine
+// safari = javascript core engine JSC
+
+// node = v8 engine
+
+// var name = "Kartik";
+
+// function fun1() {
+//     let a = 10;
+//     console.log(a);
+// }
+
+// fun1();
+// console.log(name);
+
+// call stack forms when script runs
+
+
+// Synchronous js
+// js is single threaded language
+
+// console.log("start");
+
+// let age = 30;
+
+// if (age > 18) {
+//     console.log("Person is eligible to vote");
+// }
+
+// else {
+//     console.log("Person is not eligible to vote");
+// }
+
+// function greet() {
+//     console.log("Welcome to Chitkara");
+// }
+
+// greet();
+
+// Asynchronous Javascript
+
+let a = 20;
+
+// setTimeout(() => {
+//     console.log("Inside setTimeout");
+// }, 2000);
+
+setTimeout(function() {
+    console.log("faaaaaaaaaahhhhh");
+}, 4000);
+
+console.log(a);
+
