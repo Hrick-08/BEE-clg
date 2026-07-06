b1cee6aa269621037b4a16613ced09764351749f
Hrick <hritabratadas8@gmail.com>
2026-07-06 20:02:27 +0530

    day3 with h/w nd assignment

---
+++ Day3/index.js
@@ -0,0 +1,50 @@
+// var a = 10;
+// var a = 100;
+// console.log(a);
+
+// let b = 10;
+// // let b = 100;
+// console.log(b);
+
+// const c = 10;
+// // c = 100;
+// console.log(c);
+
+// //ternary
+// let x = 10
+// x>5 ? console.log(">5") : console.log("<5")
+
+// //comparison
+// let y = 100;
+// if(y=="100") console.log(true)
+// else console.log(false)
+
+// if(y==="100") console.log(true)
+// else console.log(false)
+
+// if(y===100) console.log(true)
+// else console.log(false)
+
+// function add(x1, x2){
+//     return x1+x2;
+// }
+
+// var add2=(x1, x2) =>{
+//     return x1+x2;
+// }
+
+// console.log(add(1,2));
+// console.log(add2(1,2));
+
+// let arr = [1,2,3,4];
+// console.log(arr);
+// console.log(arr[0]);
+
+
+let obj = {
+    "name" : "Hrick",
+    "age" : "20"
+};
+
+console.log(obj);
+
