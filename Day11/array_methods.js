047286f5e669a4c606fae06a289e1437dec54f76
Hrick <hritabratadas8@gmail.com>
2026-07-23 18:06:59 +0530

    day10

---
+++ Day11/array_methods.js
@@ -0,0 +1,10 @@
+// let a = ["HTML","CSS","JS","React"]
+// console.log(a.length) //4
+
+// let b = ["HTML","CSS","JS","React"]
+// let res = b.toString() // to prinit arr as a string
+// console.log(res)
+
+let a = ["HTML", "CSS", "JS", "React"];
+console.log(a.join('|'));
+
