e3c360b371bd46151c44661fdc2865aebb9ccdeb
Hrick <hritabratadas8@gmail.com>
2026-07-27 19:59:28 +0530

    day11

--- Day11/array_methods.js
+++ Day11/array_methods.js
@@ -2,9 +2,42 @@
 // console.log(a.length) //4
 
 // let b = ["HTML","CSS","JS","React"]
-// let res = b.toString() // to prinit arr as a string
+// let res = b.toString() // to get array as a string
 // console.log(res)
 
-let a = ["HTML", "CSS", "JS", "React"];
-console.log(a.join('|'));
+// let a = ["HTML", "CSS", "JS", "React"];
+// console.log(a.join('|'));
+
+// let a1 = [11, 22, 33, 44, 55];
+// let a2 = [66, 77, 88, 99, 100];
+// let a3 = [55, 66, 77, 88, 99];
+// let res = a1.concat(a2, a3);
+// console.log(res);
+
+// let arr = [11, 22, 33, 44, 55];
+// arr.unshift(1, 2, 3, 4, 5); // add elements at the start of the array
+// console.log(arr);
+
+// let arr = [11, 22, 33, 44, 55];
+// // arr.shift(); // remove first element from the array
+// arr.pop(); // remove last element from the array
+// console.log(arr);
+
+// let arr = [11, 22, 33, 44, 55];
+// // arr.splice(1, 2); // remove elements from the array
+// arr.splice(1, 2, 100, 200, 300); // remove and add elements in the array
+// console.log(arr);
+
+// let arr=[25,50,75,100,125]
+
+// arr.splice(1,3)// (a,b) -->a-->index, b--> number of elements
+// console.log(arr)
+
+// arr.splice(1,0,5,6,7)//(a,b..c)--> c elements will get add in arr 
+
+// console.log(arr)
+
+let arr = [11, 22, 33, 44, 55];
+console.log(arr.slice(1, 4)); // (a,b) --> a-->start index, b-->end index
+
 
